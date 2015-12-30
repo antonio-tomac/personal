04e43ffad17e8500a3c460ad327a1fdaa1a21e30
Antonio Tomac <antonio.tomac1989@gmail.com>
2015-12-30 20:35:41 +0100

    add javadoc on interface method

--- MyProject/src/main/java/tomac/myproject/Processor.java
+++ MyProject/src/main/java/tomac/myproject/Processor.java
@@ -6,5 +6,11 @@ package tomac.myproject;
  * @author Antonio Tomac <antonio.tomac@mediatoolkit.com>
  */
 public interface Processor {
+	
+	/**
+	 * method processes given text
+	 * @param text
+	 * @return 
+	 */
     String process(String text);
 }
